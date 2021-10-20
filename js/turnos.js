a371eed19dbe7ac71a78719b926d227f6c221700
benjapaz57 <89224883+benjapaz57@users.noreply.github.com>
2021-10-20 18:48:47 -0300

    Pre entrega N°2

--- js/turnos.js
+++ js/turnos.js
@@ -1,33 +1,64 @@
+// Pacientes
 class Turno {
-    constructor ({ nombre, dia, hora }){
+    constructor ({ 
+        nombre, 
+        dia, 
+        hora, 
+    }){
         this.nombre = nombre;
         this.dia = dia;
         this.hora = hora;
     }
 }
 
+//VARIABLES
+let listaTurnos = [];
 
+// FUNCIONES
 const guardarDatos = () => {
-const turnos = new Turno(
-    {
-        nombre : document.getElementById("nombre").value,
-        dia : document.getElementById("dia").value,
-        hora : document.getElementById("hora").value,
-    })
+    const turnos = new Turno(
+        {
+            nombre : document.getElementById("nombre").value,
+            dia : document.getElementById("dia").value,
+            hora : document.getElementById("hora").value,
+        })
 
     document.getElementById("guardado").innerHTML += `
+    <p>${turnos.nombre}</p>
     <p>${turnos.dia}</p>
     <p>${turnos.hora}</p>
     `
-
+    console.log(turnos)
     return turnos 
 }
 
-const boton = document.getElementById("btn")
+const agendarTurno = (lista) => {
+    lista.push(guardarDatos())
+    return lista
+}
 
-boton.addEventListener("click", (e) => {
+//verificador
+const verificarTurno = () => {
+    let info;
+    if (localStorage.getItem("listaTurnos") != null){
+        info = agendarTurno(JSON.parse(localStorage.getItem("listaTurnos")))
+        return info
+    }else {
+        agendarTurno(listaTurnos)
+        info = listaTurnos
+        return info
+    }
+}
+//guardar datos
+const almacenarDatos = () => {
+    listaTurnos = verificarTurno()
+    localStorage.setItem("listaTurnos", JSON.stringify(listaTurnos))
+}
+
+
+
+// EVENTOS
+document.getElementById("btn").addEventListener("click", (e) => {
     e.preventDefault()
-    console.log(nombre.value)
-    console.log(dia.value)
-    console.log(hora.value)
+    almacenarDatos()
 })
